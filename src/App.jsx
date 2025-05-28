31f9d6767b8da13e72e028430fd62343bee8f0e1
Eduardo <eduardobullonvera@gmail.com>
2025-05-28 14:40:19 -0500

    Add App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import "./Style/globals.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
